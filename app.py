00c86e3526e1918f290088b23c77a95840735bad
semra06 <108343622+semra06@users.noreply.github.com>
2025-04-09 21:37:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Merhaba, Docker!"
+    return jsonify({
+        "message": "Merhaba Pair9,SalesPredictionApiV1 uygulaması  Docker'da çalışıyor!",
+        "versiyon": "v1.0",
+        "durum": "Her şey yolunda ✅"
+    })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
